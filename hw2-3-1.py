f0608dfc891af54acd6135c0b6e95a6dac5e0678
p22rlugo <p22rlugo@fairfieldprep.org>
2021-09-20 10:18:48 -0400

    Add file

---
+++ hw2-3-1.py
@@ -0,0 +1,9 @@
+# Ryan Lugo: RJL 9/20/21
+
+print("```")
+print("PPPPP   Y   Y   TTTTT   H   H   OOOOO   N   N")
+print("P   P    Y Y      T     H   H   O   O   NN  N")
+print("PPPPP     Y       T     HHHHH   O   O   N N N")
+print("P         Y       T     H   H   O   O   N  NN")
+print("P         Y       T     H   H   OOOOO   N   N")
+print("```")
---
+++ hw2-3-2.py
@@ -0,0 +1,9 @@
+# Ryan Lugo: RJL 9/20/21
+
+r = 4.6
+
+
+circle_area = 3.14 * r ** 2
+circle_perimeter = 2 * 3.14 * r
+
+print(circle_area,circle_perimeter)
