e692cce501c15fe8d3704602efa35331806a7734
p22rlugo <p22rlugo@fairfieldprep.org>
2021-09-27 09:56:19 -0400

    Add file

--- hw2-3-3.py
+++ hw2-3-3.py
@@ -4,6 +4,6 @@ width = 13.9
 height = 8.2
 
 rectangle_area = width * height
-rectangle_perimeter = 2*(width+height)
+rectangle_perimeter = (2*(width+height))
 
 print(rectangle_area,rectangle_perimeter)
